d1d880af424d2daa818a4f448a9ff3908a2bcba7
J.Pablo <elfio@hiperones.es>
2017-07-24 20:25:48 +0200

    Improved DB. Still missing nighttime lines.

--- qml/gen_database.py
+++ qml/gen_database.py
@@ -27,15 +27,21 @@ class populate_db:
             return 0
 
         if not self.db.execute('''CREATE TABLE IF NOT EXISTS nodes
-            (code INTEGER, name TEXT, latitude REAL, longitude REAL, altitude REAL, line_codes TEXT)'''):
+            (code INTEGER UNIQUE PRIMARY KEY, name TEXT, latitude REAL, longitude REAL, altitude REAL, line_codes TEXT)'''):
+            return 0
+        
+        if not self.db.execute('''CREATE TABLE IF NOT EXISTS line_nodes
+            (id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, line_code INTEGER NOT NULL, line_label TEXT, section INTEGER, nodes TEXT, distance INTEGER)'''):
             return 0
         
         if not self.db.execute('''CREATE TABLE IF NOT EXISTS usual_nodes
             (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, code INTEGER, custom_label TEXT, custom_color TEXT)'''):
             return 0
+        
         if not self.db.execute('''CREATE TABLE IF NOT EXISTS metadata
             (field TEXT, data TEXT)'''):
             return 0
+        
         return 1
     
     def tussam_connect(self):
@@ -50,7 +56,7 @@ class populate_db:
                 self.db.execute("SELECT * FROM lines WHERE label = ?",(line['label'],))
                 if self.db.fetchone() == None:
                     category = '2regular'
-                    if 'Circular' in line['nombre']:
+                    if line['label']  in ['C1', 'C2', 'C3', 'C4', 'C5', 'C6A']:
                         category = '0circular'
                     elif line['label'] in ['B3','B4','EA','52','53']:
                         category = '4especial'
@@ -93,11 +99,60 @@ class populate_db:
                     print("Adding node",node['codigo'])
                     self.db.execute('INSERT INTO nodes VALUES (?,?,?,?,?,?)',data)
                 
+    def get_tussam_nodes2(self):
+        self.tussam_connect()
+        self.db.execute("SELECT label, code, head_number, tail_number FROM lines")
+        for line, code, head, tail in self.db.fetchall():
+            for section in [head,tail]:
+                if section != None:
+                    node_list = []
+                    distance_list = []
+                    way = str(code)+"."+str(section)
+                    raw_data = self.tussam.service.getNodosSeccion(code,section)
+                    if len(raw_data) > 0:
+                        for remote_node in raw_data[0]:
+                            node_list.append(str(remote_node['codigoNodo']))
+                            distance_list.append(str(remote_node['distancia']))
+                            self.db.execute("SELECT * FROM nodes WHERE code = ?",(remote_node[0],))
+                            local_node = self.db.fetchone()
+                            if local_node != None:
+                                # Already existing node
+                                coincident_lines = local_node[-1].strip(":").split(":")
+                                if way not in coincident_lines:
+                                    coincident_lines.append(way)
+                                    #print(":"+":".join(coincident_lines)+":")
+                                    print("Existing node:", local_node)
+                                    print("\tUpdating line_codes to:", coincident_lines)
+                                    self.db.execute('UPDATE nodes SET line_codes=? WHERE code=?',(":"+":".join(coincident_lines)+":",remote_node[0],))
+                                else:
+                                    #print("\tNothing to update.")
+                                    pass
+                            else:
+                                # New node
+                                stop_name = remote_node['descripcion']
+                                if '(' in stop_name and not ')' in stop_name:
+                                    if 'Castillo de Marcheni' in stop_name:
+                                        stop_name += 'lla)'
+                                    else:
+                                        stop_name += ')'
+                                data = (int(remote_node['codigoNodo']), stop_name, remote_node['posicion']['latitud'], remote_node['posicion']['longitud'], remote_node['posicion']['altura'], ":"+way+":")
+                                print("Adding new node:", data,"\n")
+                                self.db.execute('INSERT INTO nodes VALUES (?,?,?,?,?,?)',data)
+                                pass
+                    data = (code, line, int(section), ":"+":".join(node_list)+":",":"+":".join(distance_list)+":",)
+                    self.db.execute('SELECT * FROM line_nodes WHERE line_code=? AND line_label=? AND section=? AND nodes=? AND distance=?', data)
+                    if self.db.fetchone() == None:
+                        print("Adding data:", data)
+                        self.db.execute('INSERT INTO line_nodes VALUES (NULL,?,?,?,?,?)', data)
+                self.db_conn.commit()
                 
-    
+                        
+        
+        
+        
     
 if __name__ == '__main__':
     x = populate_db()
     x.get_tussam_lines()
-    #x.get_tussam_nodes()
+    x.get_tussam_nodes2()
     x.close_db()
