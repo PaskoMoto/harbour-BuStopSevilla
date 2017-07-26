d85ec229714e93e476b37a9f67e0093e0c5ae745
J.Pablo <elfio@hiperones.es>
2017-07-26 18:23:35 +0200

    Results are sorted from less to more time remaining.

--- qml/api.py
+++ qml/api.py
@@ -9,17 +9,19 @@ def getTiemposLlegada(parada = 154):
     con = Client("http://www.infobustussam.com:9005/InfoTusWS/services/InfoTus?wsdl", username="infotus-usermobile", password="2infotus0user1mobile2", headers={"deviceid":str(uuid.uuid4())})
     result = con.service.getTiemposNodo(parada)
     output = []
-    #test = ""
+    fastest = 200
     for item in result.lineasCoincidentes.tiempoLinea:
         line = [item.label]
         line.append(str(abs(item.estimacion1.minutos)))
         line.append(str(abs(item.estimacion1.metros)))
-        #test = str(abs(item.estimacion1.minutos))
         line.append(str(abs(item.estimacion2.minutos)))
         line.append(str(abs(item.estimacion2.metros)))
-        output.append(line)
+        if abs(item.estimacion1.minutos) <= fastest:
+            fastest = abs(item.estimacion1.minutos) 
+            output.insert(0,line)
+        else:
+            output.append(line)
     
     pyotherside.send('TiemposLlegada',output)
-    #pyotherside.send('TiemposLlegada',test)
     print(output)
     return 1
